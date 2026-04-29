cd00438e35aa66c474feb5c0102a22c33c936974
Loluva <alvaromunive99@hotmail.com>
2026-04-29 09:52:05 -0500

    feat(config): add JWT configuration to environment variables

--- src/config/env.ts
+++ src/config/env.ts
@@ -6,7 +6,9 @@ const config={
      POSTGRES_USER : process.env.POSTGRES_USER,
      POSTGRES_PASSWORD : process.env.POSTGRES_PASSWORD,
      POSTGRES_PORT : process.env.POSTGRES_PORT,
-     API_PORT : process.env.API_PORT
+     API_PORT : process.env.API_PORT,
+     JWT_SECRET: process.env.JWT_SECRET,
+     JWT_EXPIRES_IN:process.env.JWT_EXPIRES_IN   
 }
 
 const validateEnv =function<T extends Record<string,string|undefined>>(envVars: T):{[K in keyof T]:string}{
